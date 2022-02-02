e6e611450e057ba907a195f4c49f681565122e94
ZHANG-YU-CHENG <nopony2352676@gmail.com>
2022-02-02 06:13:52 -0800

    leetcode 104 [easy] : Maximum Depth of Binary Tree 1. use googletest as unit test framework    (build code environment must have installed googletest)

--- easy/104_Maximum_Depth_of_Binary_Tree/test_104.c
+++ easy/104_Maximum_Depth_of_Binary_Tree/test_104.c
@@ -1,20 +1,56 @@
+#include "gtest/gtest.h"
+
+/*for unit test*/
+extern "C" {
 #include "binary_tree_utils.h"
+}
 
 extern int maxDepth(struct TreeNode* root);
 
-int main()
-{
-    char* arr[] = {"3","9","20","x","x","15","7"}; //expected output = 3
-    // char* arr[] = {"1","x","2"}; //expected output = 2
-    // char* arr[] = {}; //expected output = 0
-    // char* arr[] = {"0"}; //expected output = 1
+
+TEST(maxDepthTest, test1) {
+    char* arr[] = {(char*)"3",(char*)"9",(char*)"20",(char*)"x",(char*)"x",(char*)"15",(char*)"7"}; //expected output = 3
     int arrlen = (int) (sizeof(arr)/sizeof(arr[0]));
     struct TreeNode* root = NULL;
     root = arr2BT(arr, arrlen);
-
     int max_depth = 0;
     max_depth =  maxDepth(root);
-    printf("maxDepth=%d\n", max_depth);
+    EXPECT_EQ(3, max_depth);
+}
 
-    return 0;
+TEST(maxDepthTest, test2) {
+    char* arr[] = {(char*)"1",(char*)"x",(char*)"2"}; //expected output = 2
+    int arrlen = (int) (sizeof(arr)/sizeof(arr[0]));
+    struct TreeNode* root = NULL;
+    root = arr2BT(arr, arrlen);
+    int max_depth = 0;
+    max_depth =  maxDepth(root);
+    EXPECT_EQ(2, max_depth);
+}
+
+TEST(maxDepthTest, test3) {
+    char* arr[] = {}; //expected output = 0
+    int arrlen = (int) (sizeof(arr)/sizeof(arr[0]));
+    struct TreeNode* root = NULL;
+    root = arr2BT(arr, arrlen);
+    int max_depth = 0;
+    max_depth =  maxDepth(root);
+    EXPECT_EQ(0, max_depth);
+}
+
+TEST(maxDepthTest, test4) {
+    char* arr[] = {(char*)"0"}; //expected output = 1
+    int arrlen = (int) (sizeof(arr)/sizeof(arr[0]));
+    struct TreeNode* root = NULL;
+    root = arr2BT(arr, arrlen);
+    int max_depth = 0;
+    max_depth =  maxDepth(root);
+    EXPECT_EQ(1, max_depth);
+}
+
+
+int main(int argc, char **argv) {
+    printf("Running main() from %s\n", __FILE__);
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
