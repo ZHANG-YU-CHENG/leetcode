16adb4f6dc62241fbbc5f0a34412b99d639789ed
ZHANG-YU-CHENG <nopony2352676@gmail.com>
2022-02-03 20:05:09 -0800

    leetcode 110 [easy] : Balanced Binary Tree 1. add unit test using googletest framework   (must have googletest installed in build code environment)

--- easy/110_Balanced_Binary_Tree/test_110.c
+++ easy/110_Balanced_Binary_Tree/test_110.c
@@ -1,15 +1,46 @@
+#include "gtest/gtest.h"
+
+/*for unit test*/
+extern "C" {
 #include "binary_tree_utils.h"
+}
 
 extern bool isBalanced(struct TreeNode* root);
 
-int main()
-{
-    // char* arr[] = {"3","9","20","x","x","15","7"}; //expected output = true
-    // char* arr[] = {"1","2","2","3","3","x","x","4","4"}; //expected output = false
-    // char* arr[] = {}; //expected output = true
-    char* arr[] = {"1","2","2","3","x","x","3","4","x","x","4"}; //expected output = false
+TEST(isBalancedTest, test1) {
+    char* arr[] = {(char*)"3",(char*)"9",(char*)"20",(char*)"x",(char*)"x",(char*)"15",(char*)"7"}; //expected output = true
     int arrlen = (int) (sizeof(arr)/sizeof(arr[0]));
     struct TreeNode* root = NULL;
     root = arr2BT(arr, arrlen);
-    printf("is balanced ? %s\n", isBalanced(root) ? "true" : "false" );
+    EXPECT_TRUE(isBalanced(root));
+}
+
+TEST(isBalancedTest, test2) {
+    char* arr[] = {(char*)"1",(char*)"2",(char*)"2",(char*)"3",(char*)"3",(char*)"x",(char*)"x",(char*)"4",(char*)"4"}; //expected output = false
+    int arrlen = (int) (sizeof(arr)/sizeof(arr[0]));
+    struct TreeNode* root = NULL;
+    root = arr2BT(arr, arrlen);
+    EXPECT_FALSE(isBalanced(root));
+}
+
+TEST(isBalancedTest, test3) {
+    char* arr[] = {}; //expected output = true
+    int arrlen = (int) (sizeof(arr)/sizeof(arr[0]));
+    struct TreeNode* root = NULL;
+    root = arr2BT(arr, arrlen);
+    EXPECT_TRUE(isBalanced(root));
+}
+
+TEST(isBalancedTest, test4) {
+    char* arr[] = {(char*)"1",(char*)"2",(char*)"2",(char*)"3",(char*)"x",(char*)"x",(char*)"3",(char*)"4",(char*)"x",(char*)"x",(char*)"4"}; //expected output = false
+    int arrlen = (int) (sizeof(arr)/sizeof(arr[0]));
+    struct TreeNode* root = NULL;
+    root = arr2BT(arr, arrlen);
+    EXPECT_FALSE(isBalanced(root));
+}
+
+int main(int argc, char **argv) {
+    printf("Running main() from %s\n", __FILE__);
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
