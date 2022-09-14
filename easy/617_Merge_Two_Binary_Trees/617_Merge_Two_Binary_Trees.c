761806cc00ad10d97bc28e692934ebd014122f34
ZHANG-YU-CHENG <nopony2352676@gmail.com>
2022-09-14 23:44:46 +0800

    leetcode 617 [easy] : Merge Two Binary Trees 1. add stack solution and queue solution.

--- easy/617_Merge_Two_Binary_Trees/617_Merge_Two_Binary_Trees.c
+++ easy/617_Merge_Two_Binary_Trees/617_Merge_Two_Binary_Trees.c
@@ -34,6 +34,8 @@
     struct TreeNode *right;
  };
 
+/*
+// recursive solution
 struct TreeNode* mergeTrees(struct TreeNode* root1, struct TreeNode* root2){
     if(!root1)
         return root2;
@@ -44,4 +46,221 @@ struct TreeNode* mergeTrees(struct TreeNode* root1, struct TreeNode* root2){
     root1->right = mergeTrees(root1->right, root2->right);
     return root1;
 }
+*/
 
+/*
+// ==================stack utils=================== 
+struct mTreeNode{
+    struct TreeNode* val1;
+    struct TreeNode* val2;
+};
+
+typedef struct stackNode{
+    struct mTreeNode* val;
+    struct stackNode* next;
+}StackNode;
+
+typedef struct{
+    StackNode* top;
+}Stack;
+
+Stack* createStack(void){
+    Stack* s = (Stack*)malloc(sizeof(Stack));
+    s->top = NULL;
+    return s;
+}
+
+bool stackIsEmpty(Stack* s){
+    return (s==NULL || s->top==NULL);
+}
+
+void stackPush(Stack* s, struct TreeNode* val1, struct TreeNode* val2){
+    StackNode* newStackNode = (StackNode*)malloc(sizeof(StackNode));
+    newStackNode->val = (struct mTreeNode*)malloc(sizeof(struct mTreeNode));
+    newStackNode->val->val1 = val1;
+    newStackNode->val->val2 = val2;
+    newStackNode->next = s->top;
+    s->top = newStackNode;
+}
+
+struct mTreeNode* stackTop(Stack* s){
+    if(s==NULL || s->top==NULL)
+    {
+        // puts("stack is empty");
+        return NULL;
+    }
+    return s->top->val; 
+}
+
+void stackPop(Stack* s){
+    if(s==NULL || s->top==NULL)
+    {
+        // puts("stack is empty");
+        return;
+    }
+    StackNode* tmp = s->top;
+    s->top = s->top->next;
+    if(tmp) free(tmp);
+    tmp = NULL;
+}
+
+void freeStack(Stack* s){
+    while(!stackIsEmpty(s))
+        stackPop(s);
+    free(s);
+    s = NULL;
+}
+// ================================================
+
+// stack solution
+struct TreeNode* mergeTrees(struct TreeNode* root1, struct TreeNode* root2){
+    if(!root1)
+        return root2;
+    if(!root2)
+        return root1;
+    Stack* s = createStack();
+    stackPush(s, root1, root2);
+    struct mTreeNode* tmp;
+    while(!stackIsEmpty(s))
+    {
+        tmp = stackTop(s);
+        stackPop(s);
+
+        if(tmp->val1 && tmp->val2)
+        {
+            tmp->val1->val += tmp->val2->val;
+
+            if(tmp->val1->left && tmp->val2->left)
+                stackPush(s, tmp->val1->left, tmp->val2->left);
+            else if(!tmp->val1->left && tmp->val2->left)
+                tmp->val1->left = tmp->val2->left;
+
+            if(tmp->val1->right && tmp->val2->right)
+                stackPush(s, tmp->val1->right, tmp->val2->right);
+            else if(!tmp->val1->right && tmp->val2->right)
+                tmp->val1->right = tmp->val2->right;
+        }
+    }
+    return root1;
+}
+*/
+
+// ==================queue utils=================== 
+struct mTreeNode{
+    struct TreeNode* val1;
+    struct TreeNode* val2;
+};
+
+struct queueNode{
+    struct mTreeNode* val;
+    struct queueNode* pre;
+    struct queueNode* next;
+};
+
+typedef struct{
+    struct queueNode* head;
+    struct queueNode* tail;
+}Queue;
+
+Queue* createQueue(void){
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    q->head = NULL;
+    q->tail = NULL;
+    return q;
+}
+
+bool queueIsEmpty(Queue* q){
+    return (q==NULL || q->head==NULL);
+}
+
+void queuePush(Queue* q, struct TreeNode* val1, struct TreeNode* val2){
+    struct queueNode* newQueueNode = (struct queueNode*)malloc(sizeof(struct queueNode));
+    newQueueNode->val = (struct mTreeNode*)malloc(sizeof(struct mTreeNode));
+    newQueueNode->val->val1 = val1;
+    newQueueNode->val->val2 = val2;
+    if(queueIsEmpty(q))
+    {
+        newQueueNode->next = NULL;
+        newQueueNode->pre = NULL;
+        q->head = newQueueNode;
+        q->tail = newQueueNode;
+    }
+    else
+    {
+        newQueueNode->next = q->head;
+        newQueueNode->pre = NULL;
+        q->head->pre = newQueueNode;
+        q->head = newQueueNode;        
+    }
+}
+
+struct mTreeNode* queueTail(Queue* q){
+    if(q==NULL || q->tail==NULL)
+    {
+        // queue is empty
+        return NULL;
+    }
+    return q->tail->val; 
+}
+
+void queuePop(Queue* q){
+    if(q==NULL || q->tail==NULL)
+    {
+        // queue is empty
+        return;
+    }
+    struct queueNode* tmp = q->tail;
+    if(q->tail->pre==NULL) //last node
+    {
+        q->head = NULL;
+        q->tail = NULL;
+    }
+    else
+    {
+        q->tail = q->tail->pre;
+        q->tail->next = NULL;
+    }
+    if(tmp) free(tmp);
+    tmp = NULL;
+}
+
+void freeQueue(Queue* q){
+    while(!queueIsEmpty(q))
+        queuePop(q);
+    free(q);
+    q = NULL;
+}
+
+// ================================================ 
+
+// queue solution
+struct TreeNode* mergeTrees(struct TreeNode* root1, struct TreeNode* root2){
+    if(!root1)
+        return root2;
+    if(!root2)
+        return root1;
+    Queue* q = createQueue();
+    queuePush(q, root1, root2);
+    struct mTreeNode* tmp;
+    while(!queueIsEmpty(q))
+    {
+        tmp = queueTail(q);
+        queuePop(q);
+
+        if(tmp->val1 && tmp->val2)
+        {
+            tmp->val1->val += tmp->val2->val;
+
+            if(tmp->val1->left && tmp->val2->left)
+                queuePush(q, tmp->val1->left, tmp->val2->left);
+            else if(!tmp->val1->left && tmp->val2->left)
+                tmp->val1->left = tmp->val2->left;
+
+            if(tmp->val1->right && tmp->val2->right)
+                queuePush(q, tmp->val1->right, tmp->val2->right);
+            else if(!tmp->val1->right && tmp->val2->right)
+                tmp->val1->right = tmp->val2->right;
+        }
+    }
+    return root1;
+}
