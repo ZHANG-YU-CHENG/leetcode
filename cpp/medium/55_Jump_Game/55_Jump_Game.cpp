cc7545223603a4db5efd0cfed050d810f541c070
ZHANG-YU-CHENG <nopony2352676@gmail.com>
2023-05-22 20:37:20 +0800

    leetcode 55 [medium] : Jump Game 1. update DP solution

--- cpp/medium/55_Jump_Game/55_Jump_Game.cpp
+++ cpp/medium/55_Jump_Game/55_Jump_Game.cpp
@@ -27,6 +27,25 @@
 #include <vector>
 using namespace std;
 
+//DP sol, time limit exceed
+/*
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> dp(n, false);
+        dp[0] = true;
+        for(int i=1; i<n; ++i) {
+            for(int j=0; j<i; ++j) {
+                if(dp[j] && (j+nums[j] >= i))
+                    dp[i] = true;
+            }
+        }
+        return dp[n-1];
+    }
+};
+*/
+
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
